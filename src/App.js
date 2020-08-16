176d2df8cb9a4f90fed6335cf429886fc7dc89fa
Casazzan <nrcasazza@gmail.com>
2020-08-16 11:33:46 -0500

    Add search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
 import React from 'react'
+import SearchBar from './Components/SearchBar'
 
 class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            loading: false,
+            weatherData: false,
+        }
+    }
+
+    searchFromInput(input) {
+
+    }
+
+    async fetchData(city) {
+        this.setState({
+            loading: true,
+        })
+        try {
+            const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=1ba3cf989b72215565491bbe5ea27fa3');
+            const allData = await response.json();
+            const newWeatherData = {
+                name: allData.name,
+                temp: (allData.main.temp - 273.15).toFixed(1), //in C
+                feelsLike: (allData.main.feels_like - 273.15).toFixed(1),
+                iconDescription: allData.weather[0].main,
+                description: allData.weather[0].description,
+            }
+            this.setState({
+                loading: false,
+                weatherData: newWeatherData
+            })
+        }
+        catch (err) {
+            this.setState({
+                city,
+            })
+        }
+    }
+
     render() {
+        let mainContent;
+        if (this.state.loading) {
+            mainContent = <div>Loading...</div>;
+        }
+        else if (this.state.weatherData) {
+            mainContent = <div>{this.state.weatherData.name} Fi</div>;
+        }
+        else if (this.state.city) {
+            mainContent = <div>{this.state.city} was not found</div>;
+        }
         return (
-            <div>Hi</div>
+            <div id="main">
+                <img src="gifs/Clear.svg" alt="" />
+                <SearchBar searchFunction={(city) => this.fetchData(city)} />
+                {mainContent}
+            </div>
         )
     }
 }
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function SearchBar(props) {
+    function onKeyPressed(e) {
+        if (e.key === 'Enter') {
+            props.searchFunction(e.currentTarget.value);
+            e.currentTarget.value = "";
+        }
+    }
+
+    return (
+        <div id="search-container">
+            <input
+                type="text"
+                id="search-bar"
+                onKeyDown={onKeyPressed}
+                placeholder="City name"
+            >
+            </input>
+        </div>
+    )
+}
+
+export default SearchBar
