8ff1ae0c19ec947fd0577f111cdbac8a21f90ce9
Casazzan <nrcasazza@gmail.com>
2020-08-31 11:17:30 -0500

    Add functionality to modular switch

--- src/Components/toggle-switch/ToggleSwitch.js
+++ src/Components/toggle-switch/ToggleSwitch.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./toggle-switch.css";
 
 class ToggleSwitch extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            isOptionOne: true,
-        }
-    }
-
-    render() {
-        return (
-            <div className="toggle-switch">
-                <div className="toggle-switch-option">{this.props.optionOneString}</div>
-                <label className="switch">
-                    <input type="checkbox" />
-                    <span className="slider round"></span>
-                </label>
-                <div className="toggle-switch-option">{this.props.optionTwoString}</div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="toggle-switch">
+        <div className="toggle-switch-option">{this.props.optionOneString}</div>
+        <label className="switch">
+          <input type="checkbox" />
+          <span
+            className="slider round"
+            onClick={
+              this.props.toggleFunction
+                ? () => this.props.toggleFunction()
+                : null
+            }
+          ></span>
+        </label>
+        <div className="toggle-switch-option">{this.props.optionTwoString}</div>
+      </div>
+    );
+  }
 }
 
-export default ToggleSwitch
+export default ToggleSwitch;
